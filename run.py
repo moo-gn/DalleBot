f4bf99474485b8680fc3a3aff2bd5bc778e549b6
mhalhamdan <muhammad.alhamdan@gmail.com>
2022-08-21 17:41:28 -0400

    Download images instead of sending URLs

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
+from urllib import response
 import openai
 import discord
 from credentials import DALLE_SECRET, OPENAI_API_KEY, DISCORD_TOKEN
+# import urllib.request
+import requests
+import io
 from importlib import import_module
 DALLE = import_module("Python-DALLE.DALLE")
 
@@ -10,7 +14,7 @@ openai.api_key = OPENAI_API_KEY
 client = discord.Client()
 
 # Threshold for text moderation
-MODERATION_THRESHOLD = 0.01
+MODERATION_THRESHOLD = 0.1
 
 def validate_text(text):
 
@@ -24,8 +28,18 @@ def validate_text(text):
 
             return False
 
-
     return not output["flagged"]
+
+def download_image(image_url: str):
+    response = requests.get(image_url)
+    
+    if response.ok:
+        return  io.BytesIO(response.content)
+         
+    else:
+        raise Exception("Error in downloading image from URL")
+
+
 
 @client.event
 async def on_ready():
@@ -45,12 +59,15 @@ async def on_message(message: discord.Message):
             response = await message.reply("Generating...")
 
             try:
+                image_urls = await dalle.generate(dalle_prompt)
 
-                images = await dalle.generate(dalle_prompt)
+                image_list = []
 
-                for img in images:
+                for url in image_urls:
+                    image_list.append(download_image(url['generation']['image_path']))
 
-                    await message.channel.send(img['generation']['image_path'])
+                for idx, image in enumerate(image_list): 
+                    await message.channel.send(file=discord.File(fp=image, filename=f"{dalle_prompt}-{idx+1}.png"))
 
                 await response.edit(content="Done!")
 
@@ -58,7 +75,6 @@ async def on_message(message: discord.Message):
                 await response.edit(content=str(e))
 
         else:
-
             await message.reply("Your prompt was flagged by the system.")
 
 client.run(DISCORD_TOKEN)
