a9ac69981671760444311b25b6564658bfa04dee
mhalhamdan <muhammad.alhamdan@gmail.com>
2022-08-21 21:01:14 -0400

    Add database connection and counter

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
-from urllib import response
 import openai
 import discord
+import credentials
 from credentials import DALLE_SECRET, OPENAI_API_KEY, DISCORD_TOKEN
-# import urllib.request
 import requests
 import io
+from collections import Counter
+import datetime
+from tabulate import tabulate
 from importlib import import_module
 DALLE = import_module("Python-DALLE.DALLE")
 
@@ -16,8 +18,84 @@ client = discord.Client()
 # Threshold for text moderation
 MODERATION_THRESHOLD = 0.1
 
-def validate_text(text):
+# DB
+from pymysql import connect
+from sshtunnel import SSHTunnelForwarder
 
+# start the connection to pythonanywhere
+connection = SSHTunnelForwarder((credentials.ssh_website),
+                                ssh_username=credentials.ssh_username, ssh_password=credentials.ssh_password,
+                                remote_bind_address=(credentials.remote_bind_address, 3306),
+                             ) 
+connection.start()
+
+def db_init():
+  """
+  Connects to the remote database, returns the database and its cursor
+  """
+  # Connect
+  db = connect(
+      user=credentials.db_user,
+      passwd=credentials.db_passwd,
+      host=credentials.db_host, port=connection.local_bind_port,
+      db=credentials.db,
+  )
+
+  # Return cursor and db
+  return db.cursor(), db 
+
+def serialize_image_urls(images_urls: list):
+    return "|".join(images_urls)
+
+def add_prompt(author, prompt, image_urls, timestamp):
+
+    cursor, db = db_init()
+
+    sql = "INSERT INTO dalle(author, prompt, image_urls, timestamp) values (%s, %s, %s, %s);"
+    val = (author, prompt, image_urls, timestamp)
+
+    cursor.execute(sql, val)
+
+    db.commit()
+
+    db.close()
+
+def get_stats():
+
+    cursor, db = db_init()
+
+    stats = {}
+
+    cursor.execute("SELECT COUNT(*) FROM dalle;")
+
+    data = cursor.fetchone()
+
+    stats['runs'] = data[0]
+
+    stats['spent'] = round(15.0/115.0 * stats['runs'], 2)
+
+    cursor.execute(f"SELECT author FROM dalle;")
+
+    data = cursor.fetchall()
+
+    db.close()
+    
+    user_counts = Counter(data)
+
+    user_data = [] 
+
+    for user, count in user_counts.items():
+        val = (user[0], count, f"${round(15.0/115.0 * count, 2)}")
+        user_data.append(val)
+
+    user_data.sort(key=lambda x: x[1], reverse=True)
+
+    stats['user_data'] = user_data
+
+    return stats
+
+
+def validate_text(text):
     response = openai.Moderation.create(input=text)
 
     output = response["results"][0]
@@ -38,7 +116,6 @@ def download_image(image_url: str):
          
     else:
         raise Exception("Error in downloading image from URL")
-
 
 
 @client.event
@@ -65,16 +142,40 @@ async def on_message(message: discord.Message):
 
                 for url in image_urls:
                     image_list.append(download_image(url['generation']['image_path']))
+                
+                cdn_urls = []
 
                 for idx, image in enumerate(image_list): 
-                    await message.channel.send(file=discord.File(fp=image, filename=f"{dalle_prompt}-{idx+1}.png"))
+                    image_id = await message.channel.send(file=discord.File(fp=image, filename=f"{dalle_prompt}-{idx+1}.png"))
+
+                    cdn_urls.append(image_id.attachments[0].url)
 
                 await response.edit(content="Done!")
+                
+                # Add prompt to database
+                add_prompt(message.author, dalle_prompt, serialize_image_urls(cdn_urls), message.created_at)
 
             except Exception as e:
                 await response.edit(content=str(e))
-
+        
         else:
             await message.reply("Your prompt was flagged by the system.")
+
+    if message_text.startswith("-dalle"):
+
+        stats = get_stats()
+
+        stats_text = "```"
+
+        stats_text += f"As of {datetime.date.today().strftime('%b %d, %Y')}, total runs: {stats['runs']}, total spent: ${stats['spent']}\n"
+
+        stats_text += "\nStats breakdown\n"
+
+
+        stats_text += tabulate(stats['user_data'], headers=('User', '# Runs', 'Spent'), tablefmt='fancy_grid')
+
+        stats_text += "```"
+
+        await message.reply(stats_text)
 
 client.run(DISCORD_TOKEN)
